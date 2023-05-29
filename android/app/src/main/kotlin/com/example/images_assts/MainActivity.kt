c102d79c69e28ba2ff60c32010e92a054b62a684
Jawad <jawadmuhammad867@gmail.com>
2023-05-29 14:21:27 +0500

    final commit

---
+++ android/app/src/main/kotlin/com/example/images_assts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.images_assts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Image.asset('img/foodpanda.png', fit: BoxFit.fill,),
+            //height: 200,
+            width: 500,
+          ),
+        ),
+      ),
+    );
+  }
+}
